570da8f0a79c3ff000a12cacdf6584f984ab23eb
Trinh Dung <c170017@e.ntu.edu.sg>
2019-07-19 10:03:40 +0800

    Data binding and a simple intent

--- app/src/main/java/com/android/example/clockmaster/MainActivity.kt
+++ app/src/main/java/com/android/example/clockmaster/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.android.example.clockmaster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.android.example.clockmaster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.buttonAlarm.setOnClickListener {  startAlarmActivity() }
+    }
+
+    private fun startAlarmActivity(){
+        val alarmIntent : Intent = Intent(this, AlarmActivity::class.java)
+        startActivity(alarmIntent)
     }
 }
