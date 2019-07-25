4af1206d8a66864267cd29ff3633c57880420fef
Trinh Dung <c170017@e.ntu.edu.sg>
2019-07-25 21:17:17 +0800

    Successfully handled spinners for hour and time

--- app/src/main/java/com/android/example/clockmaster/AlarmActivity.kt
+++ app/src/main/java/com/android/example/clockmaster/AlarmActivity.kt
@@ -8,47 +8,34 @@ import android.widget.*
 
 class AlarmActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
 
-   // private lateinit var hoursSpinner: Spinner
-   // private lateinit var minutesSpinner: Spinner
-   // private lateinit var setButton: Button
+    private lateinit var hoursSpinner: Spinner
+    private lateinit var minutesSpinner: Spinner
+    private lateinit var setButton: Button
     private var isAlarmSet: Boolean = false
-    private var hourValue: Int = -1
-    private var minuteValue: Int = -1
+    private lateinit var hourValue: String
+    private lateinit var minuteValue: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
 
-        val hoursSpinner: Spinner = findViewById(R.id.hours_spinner)
-        val minutesSpinner: Spinner = findViewById(R.id.minutes_spinner)
-        val setButton: Button = findViewById(R.id.set_button)
+        hoursSpinner = findViewById(R.id.hours_spinner)
+        minutesSpinner = findViewById(R.id.minutes_spinner)
+        setButton = findViewById(R.id.set_button)
+
 
         // Create an ArrayAdapter using the string array and a default spinner layout
-        
-        ArrayAdapter.createFromResource(
-                this,
-        R.array.hours_array,
-        android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            hoursSpinner.setAdapter(adapter)
-        }
-
-
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.minutes_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            minutesSpinner.setAdapter = adapter
-        }
-
+        val hoursAdapter = ArrayAdapter.createFromResource(
+            this, R.array.hours_array, android.R.layout.simple_spinner_item)
+        hoursAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        hoursSpinner.adapter = hoursAdapter
         hoursSpinner.onItemSelectedListener = this
+
+
+        val minutesAdapter = ArrayAdapter.createFromResource(
+            this, R.array.minutes_array, android.R.layout.simple_spinner_item)
+        minutesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        minutesSpinner.adapter = minutesAdapter
         minutesSpinner.onItemSelectedListener = this
 
         setButton.setOnClickListener { setAlarm() }
@@ -59,16 +46,19 @@ class AlarmActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
 
     }
 
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
 
-        Toast.makeText(this,parent!!.id,Toast.LENGTH_SHORT).show()
-
+        when(parent.getCount()){
+            24 -> hourValue = parent.getItemAtPosition(position) as String
+            60 -> minuteValue = parent.getItemAtPosition(position) as String
+        }
     }
     private fun setAlarm(){
         if (!isAlarmSet){
             // handle the setup of clock
+
             setButton.setText(R.string.unset)
-            Toast.makeText(this,"The time set is:" + hourValue + ":" + minuteValue,Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"The time set is: " + hourValue + ":" + minuteValue,Toast.LENGTH_SHORT).show()
             isAlarmSet = true
         }
         else {
