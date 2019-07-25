5e3d278a08c6b59b47b13a854dea48962f8ea631
Trinh Dung <c170017@e.ntu.edu.sg>
2019-07-25 20:17:16 +0800

    Handle item click listener for spinner

--- app/src/main/java/com/android/example/clockmaster/AlarmActivity.kt
+++ app/src/main/java/com/android/example/clockmaster/AlarmActivity.kt
@@ -2,14 +2,19 @@ package com.android.example.clockmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.android.example.clockmaster.databinding.ActivityAlarmBinding
 
-class AlarmActivity : AppCompatActivity() {
+class AlarmActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
 
     private lateinit var binding: ActivityAlarmBinding
     private var isAlarmSet: Boolean = false
+    private var hourValue: Int = -1
+    private var minuteValue: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,7 @@ class AlarmActivity : AppCompatActivity() {
             binding.hoursSpinner.setAdapter(adapter)
         }
 
+
         ArrayAdapter.createFromResource(
             this,
             R.array.minutes_array,
@@ -38,14 +44,30 @@ class AlarmActivity : AppCompatActivity() {
             binding.minutesSpinner.setAdapter(adapter)
         }
 
+        binding.hoursSpinner.onItemSelectedListener = this
+        binding.minutesSpinner.onItemSelectedListener = this
+
         binding.setButton.setOnClickListener { setAlarm() }
 
     }
 
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (parent!!.id) {
+            24 -> hourValue = parent.getItemAtPosition(position) as Int
+            60 -> minuteValue = parent.getItemAtPosition(position) as Int
+        }
+        Toast.makeText(this,parent.count,Toast.LENGTH_SHORT).show()
+
+    }
     private fun setAlarm(){
         if (!isAlarmSet){
             // handle the setup of clock
             binding.setButton.setText(R.string.unset)
+            Toast.makeText(this,"The time set is:" + hourValue + ":" + minuteValue,Toast.LENGTH_SHORT).show()
             isAlarmSet = true
         }
         else {
